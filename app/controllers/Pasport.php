7da007849a2c1c27e50c0918fc742b166bb09544
tolik8 <tolik8@gmail.com>
2018-12-05 18:21:39 +0200

    fix

--- app/controllers/Pasport.php
+++ app/controllers/Pasport.php
@@ -111,7 +111,7 @@ class Pasport
         $this->prepareTable01($params);
         $this->prepareTable02($params);
 
-        $this->db->insert('PIKALKA.pasp_log', [
+        $this->db->insert('PIKALKA.pasp_jrn', [
             'guid' => $this->new_guid,
             'dt1' => $params['dt1'],
             'dt2' => $params['dt2'],
@@ -197,6 +197,9 @@ class Pasport
         return $prepared;
     }
 
+    /**
+     *
+     */
     public function excel ()
     {
         $templateFile = '../xls/pasport/template.xlsx';
@@ -207,39 +210,34 @@ class Pasport
         catch (\Exception $e) {echo $e->getMessage(); Exit;}
 
         try {
-            $templateVars1 = $spreadsheet->getSheet(0)->toArray();
-            $templateVars2 = $spreadsheet->getSheet(1)->toArray();
-            $templateVars3 = $spreadsheet->getSheet(2)->toArray();
+            for ($i = 0; $i <= $spreadsheet->getSheetCount()-1; $i++)
+                {$templateVars[$i+1] = $spreadsheet->getSheet($i)->toArray();}
         } catch (\Exception $e) {echo $e->getMessage(); Exit;}
 
         $pattern = '#^[0-9a-zA-Z]{32}$#';
         $this->new_guid = regex($pattern, $_POST['guid'], 0);
 
-        $params = $this->db->getOneRow('PIKALKA.pasp_log', ['guid' => $this->new_guid]);
+        $params = $this->db->getOneRow('PIKALKA.pasp_jrn', ['guid' => $this->new_guid]);
 
         $input_xlsParams = ['{tin}' => $params['TIN'], '{dt1}' => $params['DT1'], '{dt2}' => $params['DT2']];
 
+        // Реєстраційні дані
         $reg_data = $this->excelRegData($params);
         $templateParams = array_merge($input_xlsParams, $reg_data);
         try {$sheet1 = $spreadsheet->getSheet(0);}
         catch (\Exception $e) {echo $e->getMessage(); Exit;}
-        PhpExcelTemplator::renderWorksheet($sheet1, $templateVars1, $templateParams);
+        PhpExcelTemplator::renderWorksheet($sheet1, $templateVars[1], $templateParams);
 
-        $templateParams = $this->excel_r21stan_h($params);
-        try {$sheet2 = $spreadsheet->getSheet(1);}
-        catch (\Exception $e) {echo $e->getMessage(); Exit;}
-        PhpExcelTemplator::renderWorksheet($sheet2, $templateVars2, $templateParams);
-
+        // Контрагенти
         $params_01 = $this->excelTable01($params);
         $params_02 = $this->excelTable02($params);
         $templateParams = array_merge($params_01, $params_02);
-        try {$sheet3 = $spreadsheet->getSheet(2);}
+        try {$sheet2 = $spreadsheet->getSheet(1);}
         catch (\Exception $e) {echo $e->getMessage(); Exit;}
-        PhpExcelTemplator::renderWorksheet($sheet3, $templateVars3, $templateParams);
+        PhpExcelTemplator::renderWorksheet($sheet2, $templateVars[2], $templateParams);
 
         if ($outputMethod) PhpExcelTemplator::outputSpreadsheetToFile($spreadsheet, $outputFile);
         else PhpExcelTemplator::saveSpreadsheetToFile($spreadsheet, $outputFile);
-
     }
 
     public function excelRegData ($input_params)
@@ -263,6 +261,10 @@ class Pasport
         $sql = file_get_contents('../sql/pasport/get_pdv_act_r.sql');
         $pdv_act_r = $this->db->getOneRowFromSQL($sql, $params);
 
+        $sql = file_get_contents('../sql/pasport/get_r21stan_h.sql');
+        $array = $this->db->getAllFromSQL($sql, $params);
+        $stan_h = $this->transform($array);
+
         $xls_params = [
             '{r21taxpay.c_distr}' => $this->c_distr,
             '{r21taxpay.name}' => utf8($r21taxpay['NAME']),
@@ -274,29 +276,16 @@ class Pasport
             '{r21paddr.address}' => utf8($address),
             '{r21manager.dir}' => utf8($r21manager[1]['NAME']),
             '{r21manager.buh}' => utf8($r21manager[2]['NAME']),
-            '{r21manager.dir_tel}' => utf8('тел. ' . $r21manager[1]['N_TEL']),
-            '{r21manager.buh_tel}' => utf8('тел. ' . $r21manager[2]['N_TEL']),
+            '{r21manager.dir_tel}' => utf8($r21manager[1]['N_TEL']),
+            '{r21manager.buh_tel}' => utf8($r21manager[2]['N_TEL']),
             '{pdv_act_r.dat_reestr}' => $pdv_act_r['DAT_REESTR'],
-        ];
-
-        return $xls_params;
-    }
-
-    public function excel_r21stan_h ($input_params)
-    {
-        $params['TIN'] = $input_params['TIN'];
-
-        $sql = file_get_contents('../sql/pasport/get_r21stan_h.sql');
-        $array = $this->db->getAllFromSQL($sql, $params);
-        $stan_h = $this->transform($array);
-
-        $xls_params = [
             '[r21stan_h.n]' => $stan_h['N'],
             '[r21stan_h.old]' => $stan_h['OLD_STAN'],
             '[r21stan_h.new]' => $stan_h['NEW_STAN'],
             '[r21stan_h.dt]' => $stan_h['D_CHANGE'],
             '[r21stan_h.act]' => $stan_h['IS_ACTUAL'],
         ];
+
         return $xls_params;
     }
 

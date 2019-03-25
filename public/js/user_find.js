9a2af789a9d9c2a8a19b8e1920c109845a1d99de
tolik8 <tolik8@gmail.com>
2019-03-25 18:32:13 +0200

    autocomplete

--- public/js/user_find.js
+++ public/js/user_find.js
@@ -1,4 +1,5 @@
 //https://www.devbridge.com/sourcery/components/jquery-autocomplete/
+
 $('#find').devbridgeAutocomplete({
     serviceUrl: 'user_find',
     type: 'POST',
@@ -7,5 +8,22 @@ $('#find').devbridgeAutocomplete({
     onSelect: function (suggestion) {
         //console.log('You selected: ' + suggestion.value + ', ' + suggestion.data);
         $('#guid').val(suggestion.data);
+    }
+});
+
+$('#checkbox').click(function() {
+    if ($('#checkbox').prop('checked')) {
+        $('#find').autocomplete().enable();
+    } else {
+        $('#guid').val('');
+        console.log($('#find').autocomplete());
+        $('#find').autocomplete('clear');
+        $('#find').autocomplete('clearCache');
+        $('#find').autocomplete('hide');
+        // $('#find').autocomplete().clear();
+        // $('#find').autocomplete().clearCache();
+        // $('#find').autocomplete().hide();
+        $('#find').autocomplete().disable();
+        // $('#find').autocomplete().dispose();
     }
 });
